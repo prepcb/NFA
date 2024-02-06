99b2e64a4070a9126e046bb474f27db3ecd754b3
prepcb <123950776+prepcb@users.noreply.github.com>
2024-02-06 17:49:21 +0000

    graphs added topological

--- DFA_recursive.c
+++ DFA_recursive.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
-int R(int T[][2],int A[],int d){
-    if(d==0)
+// T is transition matrix; A is input string: scanned in.
+int T[100][2],A[100];
+
+//returns new state N given state N-1 and string character A[N-1] 
+int R(int n){
+    if(n==0)
         return(0);
     else
-        return(  T[R(T,A,d-1)] [A[d-1]]  );
+        return(  T[R(n-1)] [A[n-1]]  );
 }
-
 int main(){
     //Uses the formula R(n) = delta(R(n-1),A[n-1])
     // with R(0) the starting state
 
     // Input DFA and string
     printf("Input number of states\n");
-    int n,T[100][2];
+    int n;
     scanf("%d",&n);
     if(n<0)exit(0);
     printf("The states are numbered 0 to %d\n Starting state is assumed '0'\n",n-1);
@@ -27,7 +30,7 @@ int main(){
     int k =0;
     while(k==0){
     printf("Input string of 0s and 1s followed by -1\n");
-    int d,A[100];
+    int d;
     d = 0;
     scanf("%d",&A[d]);
     while(A[d]>=0){
@@ -37,7 +40,7 @@ int main(){
     ///// end of input //////
 
 
-    printf("Final state is %d\n",R(T,A,d));
+    printf("Final state is %d\n",R(d));
     }
 
 }
